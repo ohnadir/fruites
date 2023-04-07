2ba357cc14082a4837e84e0846a4796f3357e1e8
Nadir Hossain <nadirhossain336@gmail.com>
2023-04-08 02:15:06 +0600

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div>
+      <p>Railways App</p>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/Component/Navbar.tsx
@@ -0,0 +1,11 @@
+import "../StyleSheet/Navbar.scss"
+const Navbar = () => {
+  return (
+    <div>
+        <p className="header">Navbar</p>
+        
+    </div>
+  )
+}
+
+export default Navbar
